e627e63373fa6acec0d4cd348b51f5c8bdcb71f2
Dongwon Seo <52277540+kirin2211@users.noreply.github.com>
2023-03-13 12:57:15 +0900

    [unrated] Title: 둘만의 암호, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -0,0 +1,15 @@
+def solution(s, skip, index):
+    answer = ""
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    
+    for ch in skip: 
+        if ch in alpha:
+            alpha = alpha.replace(ch, "")
+    
+    print(alpha)
+    
+    for i in s:
+        change = alpha[(alpha.index(i) + index) % len(alpha)] 
+        answer += change
+    
+    return answer
