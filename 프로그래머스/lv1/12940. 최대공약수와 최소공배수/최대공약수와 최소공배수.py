9891b21a5562198d0b1832de15f5f16b25f123e3
Dongwon Seo <52277540+kirin2211@users.noreply.github.com>
2023-04-03 00:53:45 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -0,0 +1,12 @@
+def gcd(n,m):
+    while m:
+        n,m = m, n%m
+    return n
+
+def lcm(n,m):
+    return n * m / gcd(n,m)
+def solution(n, m):
+    answer = []
+    answer.append(gcd(n,m))
+    answer.append(lcm(n,m))
+    return answer
