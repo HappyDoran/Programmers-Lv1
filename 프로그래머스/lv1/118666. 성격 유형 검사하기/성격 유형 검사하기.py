85d5aa5156982284f20d617c5318f0ec15ebc783
Dongwon Seo <52277540+kirin2211@users.noreply.github.com>
2023-04-06 15:25:54 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.19 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -0,0 +1,30 @@
+def solution(survey, choices):
+    answer = ''
+    type = {"R" : 0, "T" : 0,"C" : 0,"F" : 0,"J" : 0,"M" : 0,"A" : 0,"N" : 0}
+    for i in range(len(survey)):
+        if 4 - choices[i] > 0 :
+            type[survey[i][0]]+=4-choices[i]
+        else :
+            type[survey[i][1]]+=choices[i]-4
+    
+    if type["R"] >= type["T"]:
+        answer+="R"
+    else :
+        answer+="T"
+    
+    if type["C"] >= type["F"]:
+        answer+="C"
+    else :
+        answer+="F"
+    
+    if type["J"] >= type["M"]:
+        answer+="J"
+    else :
+        answer+="M"
+    
+    if type["A"] >= type["N"]:
+        answer+="A"
+    else :
+        answer+="N"
+        
+    return answer
